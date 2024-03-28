ea5a5557652e2fe96ec4033700dfe6d6ccba9c4f
Andrew Johnson <johnsonrobertandrew@gmail.com>
2024-03-28 09:28:45 -0500

    cleanup old settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,37 +1,26 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
--- require("plugins")
+
 require("map")
 
 -- Make sure we don"t use space bar for anything else
-map {"n", "<SPACE>", "<Nop>"}
+map({ "n", "<SPACE>", "<Nop>" })
 
 -- Map <Leader> to space bar
 vim.g.mapleader = " "
 
-map {"i", "jk", "<Esc>"}
+map({ "i", "jk", "<Esc>" })
 -- space + l to go to last file in buffer
-map {"n", "<Leader>l", "<C-^>"}
+map({ "n", "<Leader>l", "<C-^>" })
 
 -- easier split navigation
-map {"n", "<C-J>", "<C-W><C-J>"}
-map {"n", "<C-K>", "<C-W><C-K>"}
-map {"n", "<C-L>", "<C-W><C-L>"}
-map {"n", "<C-H>", "<C-W><C-H>"}
+map({ "n", "<C-J>", "<C-W><C-J>" })
+map({ "n", "<C-K>", "<C-W><C-K>" })
+map({ "n", "<C-L>", "<C-W><C-L>" })
+map({ "n", "<C-H>", "<C-W><C-H>" })
 
 -- clear search highlights on escape
-map {"n", "<Esc>", ":noh<CR>"}
-
--- NERDTree
-map {"n", "<C-n>", ":NERDTreeToggle %<CR>"}
-
--- Fuzzy find
--- map {"n", "<C-p>", ":GFiles<CR>"}
--- map {"n", "<C-f>", ":Files<CR>"}
--- map {"n", "<leader>b", ":Buffers<CR>"}
--- map {"n", "<leader>f", ":Ag<CR>"}
-
--- vim.g.fzf_layout = { window = { width = 0.5, height = 0.3, relative = "v:true", yoffset = 0 } }
+map({ "n", "<Esc>", ":noh<CR>" })
 
 local set = vim.opt
 
@@ -47,14 +36,8 @@ set.tabstop = 2
 set.shiftwidth = 2
 set.expandtab = true
 
--- vim.cmd "let g:airline#extensions#tabline#enabled = 1"
-
 -- Colors
 set.termguicolors = true
--- vim.cmd "colorscheme onedark"
-
--- vim.g.airline_theme = "onedark"
--- vim.g.airline_section_z = "%l:%c"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
