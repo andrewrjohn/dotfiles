9d05e95cab9a856d3b4b1631cfed8a764dc48001
Andrew Johnson <johnsonrobertandrew@gmail.com>
2024-04-01 15:08:51 -0500

    rust formatting

--- .config/nvim/lua/plugins/commentary.lua
+++ .config/nvim/lua/plugins/commentary.lua
@@ -1,3 +1,31 @@
 return {
-  "tpope/vim-commentary",
+	"tpope/vim-commentary",
+	config = function()
+		vim.g["context#commentstring#table"] = {
+			["javascript.jsx"] = {
+				jsComment = "// %s",
+				jsImport = "// %s",
+				jsxStatement = "// %s",
+				jsxRegion = "{/*%s*/}",
+			},
+			["javascriptreact"] = {
+				jsComment = "// %s",
+				jsImport = "// %s",
+				jsxStatement = "// %s",
+				jsxRegion = "{/*%s*/}",
+			},
+			["typescript.tsx"] = {
+				tsComment = "// %s",
+				tsImport = "// %s",
+				tsxStatement = "// %s",
+				tsxRegion = "{/*%s*/}",
+			},
+			["typescriptreact"] = {
+				tsComment = "// %s",
+				tsImport = "// %s",
+				tsxStatement = "// %s",
+				tsxRegion = "{/*%s*/}",
+			},
+		}
+	end,
 }
