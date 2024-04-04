02dfab69720bc95fc10466ddb6eff3b1d54551ce
Andrew Johnson <johnsonrobertandrew@gmail.com>
2024-04-04 15:09:31 -0500

    updated toggleterm hotkey so no lag when typing space in terminal

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,17 +1,16 @@
 return {
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	config = function()
-		require("toggleterm").setup({
-			open_mapping = [[<leader>>]],
-			size = 20,
-			persist_size = true,
-			insert_mappings = true,
-			terminal_mappings = true,
-			close_on_exit = true,
-			direction = "float",
-			dir = "git_dir",
-			shade_terminals = true,
-		})
-	end,
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = [[<C-y>]],
+      size = 20,
+      persist_size = true,
+      terminal_mappings = true,
+      close_on_exit = true,
+      direction = "float",
+      dir = "git_dir",
+      shade_terminals = true,
+    })
+  end,
 }
