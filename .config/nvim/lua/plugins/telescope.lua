b41d4772ab0909bae2782fdf3d1bd21f4ff28b88
Andrew Johnson <johnsonrobertandrew@gmail.com>
2024-11-21 10:12:42 -0600

    tmux pane navigation

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,28 @@ return {
     config = function()
       local builtin = require("telescope.builtin")
 
+      local function search_and_replace()
+        local input = require("telescope.themes").get_dropdown({
+          prompt_title = "Search and Replace",
+          results_title = "Replace with:",
+          find_command = { "rg", "--files" },
+          attach_mappings = function(_, map)
+            map("i", "<CR>", function(prompt_bufnr)
+              local search = require("telescope.actions.state").get_current_line()
+              require("telescope.actions").close(prompt_bufnr)
+              local replace = vim.fn.input("Replace with: ")
+              vim.cmd(
+                string.format("cfdo %s/\\V%s/%s/g | update", search, vim.escape(search, "/"), replace)
+              )
+            end)
+            return true
+          end,
+        })
+        require("telescope.builtin").live_grep(input)
+      end
+
+      vim.keymap.set("n", "<leader>fr", search_and_replace, { noremap = true })
+
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
       vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
